fd79608e62b7c814ce79f213d0f98cc3e474ab17
孟凡雷 <mrmeng@MrMeng-2.local>
2023-06-25 18:00:56 +0800

    u

--- assets/scripts/data/SysConfig.ts
+++ assets/scripts/data/SysConfig.ts
@@ -5,7 +5,7 @@ import LongUtil from "../utils/LongUtil";
 
 class SysConfig {
     /**调试模式 */
-    readonly isDebug: number = 0;
+    readonly isDebug: number = 1;
     /**带有af统计的默认渠道值 */
     readonly defaultChannel: string = 'Organic';
     /**是否是测试环境 */
